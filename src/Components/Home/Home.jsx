52232434928c85c5335a8fb161603a905e3599dd
uiwilliams <uiwilliamsui@gmail.com>
2023-09-29 05:56:18 -0700

    Construct-demo ReactJS app

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './home.css'
+import video from '../../Assets/video1.mp4'
+import {GrLocation} from'react-icons/gr'
+
+
+const Home = () => {
+  return (
+    <section className='home'>
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+
+          <span className="smallText">
+            Our Packages
+          </span>
+
+          <h1 className="homeTitle">
+            Search your Construction Needs
+          </h1>
+          
+        </div>
+
+        <div className="cardDiv grid">
+            <div className="jobRequest">
+              <label htmlFor="city">Search your requested city:</label>
+              <div className="input flex">
+                <input type="text" placeholder='Enter name here....'/>
+                <GrLocation className="icon"/>
+              </div>
+            </div>
+
+            <div className="dateInput">
+              <label htmlFor="date">Select your date:</label>
+              <div className="input flex">
+                <input type="date" />
+              </div>
+            </div>
+
+            <div className="priceInput">
+              <div className="label_total flex">
+                <label htmlFor="price">Max price:</label>
+                <h3 className="total">$5000</h3>
+              </div>
+              <div className="input flex">
+                <input type="range" max="5000" min="100"/>
+              </div>
+            </div>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Home
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import './navbar.css'
+import {MdOutlineConstruction} from 'react-icons/md'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {TbGridDots} from 'react-icons/tb'
+
+const Navbar = () => {
+  const [active, setActive] = useState('navBar')
+  //Funtion to toggle navBar
+  const showNav = ()=>{
+    setActive('navBar')
+  }
+
+   //Funtion to remove navBar
+   const removeNavbar = ()=>{
+    setActive('navBar')
+  }
+  
+  return (
+    <section className='navBarSection'>
+      <header className="header flex">
+
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1><MdOutlineConstruction className="icon"/> Construct.</h1>
+          </a>
+        </div>
+
+        <div className={active}>
+          <ul className="navLists flex">
+
+            <li className="navItem">
+              <a href="#" className="navLink">Home</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Packages</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">About</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Services</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">News</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Contact</a>
+            </li>
+
+            <button className='btn'>
+              <a href="#">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div onClick={removeNavbar} className="closeNavbar">
+            <AiFillCloseCircle className='icon'/>
+          </div>
+        </div>
+
+        <div onClick={showNav} className="toggleNavbar">
+          <TbGridDots className='icon'/>
+        </div>
+
+      </header>
+    </section>
+  )
+}
+
+export default Navbar
