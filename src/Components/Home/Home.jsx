28424f350c99f75acf287ea691cd7fcef7276cc5
uiwilliams <uiwilliamsui@gmail.com>
2023-10-04 00:28:53 -0700

    Added Home, Main and Navbar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './home.css'
-import video from '../../Assets/video1.mp4'
+import video from '../../Assets/video (1).mp4'
 import {GrLocation} from'react-icons/gr'
 import {HiFilter} from'react-icons/hi'
 import {FiFacebook} from'react-icons/fi'
